5770a3920355799e85bc0575719c7e251cd00629
Angel <angelfernandezblanco16@gmail.com>
2025-04-06 12:49:32 +0200

    Cambios esteticos en el Menu

--- src/main/java/com/gearborn/motors/api/controllersFx/MenuController.java
+++ src/main/java/com/gearborn/motors/api/controllersFx/MenuController.java
@@ -1,5 +1,6 @@
 package com.gearborn.motors.api.controllersFx;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
 public class MenuController {
@@ -7,10 +8,13 @@ public class MenuController {
     public Label presentation;
 
     public void initialize() {
-        System.out.println("Estas en la escena -> Menu");
         presentation.setText("En GearBorn Motors no solo vendemos vehículos, conectamos personas con su próximo destino." +
-                                "Te acompañamos en cada paso: ya sea que estés comprando tu primer coche," +
-                                     "vendiendo uno de confianza o buscando una nueva experiencia al volante.");
+                "Te acompañamos en cada paso: ya sea que estés comprando tu primer coche," + 
+                "vendiendo uno de confianza o buscando una nueva experiencia al volante.");
+    }
+
+
+    public void login(ActionEvent event) {
 
     }
 }
